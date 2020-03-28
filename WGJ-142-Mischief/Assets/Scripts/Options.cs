0bf00e7cad570b5823172ee6773986b71cde99f3
benkir07 <benkir07@gmail.com>
2020-03-29 00:08:11 +0300

    Pause and Resolution
    In build I had a crash during pause in the game, no idea why and could not make it happen again.
    Should investigate it.

--- WGJ-142-Mischief/Assets/Scripts/Options.cs
+++ WGJ-142-Mischief/Assets/Scripts/Options.cs
@@ -2,12 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class Options : MonoBehaviour
 {
     public Slider musicSlider;
     public Slider soundFXSlider;
     public Slider voiceSlider;
+    public Toggle fullscreen;
+    public TMP_Dropdown resolutions;
+
+    private static List<(int, int)> res_options = new List<(int, int)>();
 
     [RuntimeInitializeOnLoadMethod]
     private static void Initialze()
@@ -15,6 +20,11 @@ public class Options : MonoBehaviour
         PlayerPrefs.SetFloat("music_volume", 1);
         PlayerPrefs.SetFloat("fx_volume", 1);
         PlayerPrefs.SetFloat("voice_volume", 1);
+        foreach (var item in Screen.resolutions)
+        {
+            if (!res_options.Contains((item.width, item.height)))
+                res_options.Add((item.width, item.height));
+        }
     }
 
     private void Start()
@@ -22,6 +32,11 @@ public class Options : MonoBehaviour
         musicSlider.value = PlayerPrefs.GetFloat("music_volume");
         soundFXSlider.value =  PlayerPrefs.GetFloat("fx_volume");
         voiceSlider.value = PlayerPrefs.GetFloat("voice_volume");
+        fullscreen.isOn = Screen.fullScreen;
+
+        resolutions.ClearOptions();
+        resolutions.AddOptions(res_options.ConvertAll((x) => x.Item1 + "x" + x.Item2));
+        resolutions.value = res_options.IndexOf((Screen.width, Screen.height));
     }
 
     private void OnGUI()
@@ -29,5 +44,8 @@ public class Options : MonoBehaviour
         PlayerPrefs.SetFloat("music_volume", musicSlider.value);
         PlayerPrefs.SetFloat("fx_volume", soundFXSlider.value);
         PlayerPrefs.SetFloat("voice_volume", voiceSlider.value);
+
+        (int, int) res = res_options[resolutions.value];
+        Screen.SetResolution(res.Item1, res.Item2, fullscreen.isOn);
     }
 }

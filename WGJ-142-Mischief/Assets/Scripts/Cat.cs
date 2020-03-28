296cd81b6a269f6f63d711ad369139432dcb7629
RazB <razbonen@gmail.com>
2020-03-28 18:53:44 +0300

    Change Sprite shape, add dust effects on collision with ground

---
+++ WGJ-142-Mischief/Assets/Scripts/Cat.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cat : MonoBehaviour {
+
+	public static Rigidbody2D rb;
+	float dirX, moveSpeed = 5f;
+
+    [SerializeField]
+    GameObject dustCloud;
+
+    bool coroutineAllowed, grounded;
+
+    // Use this for initialization
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        Debug.Log(("just collided " + col.GetContact(0).point.ToString() + " "+ col.otherCollider.transform.position.ToString() + " " + col.otherCollider.bounds.ToString()));
+
+        float delta = Mathf.Abs(col.GetContact(0).point.y - col.rigidbody.transform.position.y);
+
+        if (col.gameObject.tag.Equals("Ground") && delta >= 0.1)
+        {
+            grounded = true;
+            coroutineAllowed = true;
+            Instantiate(dustCloud, transform.position, dustCloud.transform.rotation);
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D col)
+    {
+        Debug.Log("not colliding");
+        if (col.gameObject.tag.Equals("Ground"))
+        {
+            grounded = false;
+            coroutineAllowed = false;
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		dirX = Input.GetAxisRaw ("Horizontal") * moveSpeed;
+
+		if (Input.GetButtonDown ("Jump"))
+			rb.AddForce (Vector2.up * 500f);
+
+        if (grounded && rb.velocity.x != 0 && coroutineAllowed)
+        {
+            StartCoroutine("SpawnCloud");
+            coroutineAllowed = false;
+        }
+
+        if (rb.velocity.x == 0 || !grounded)
+        {
+            StopCoroutine("SpawnCloud");
+            coroutineAllowed = true;
+        }
+    }
+
+	void FixedUpdate()
+	{
+		rb.velocity = new Vector2 (dirX, rb.velocity.y);
+	}
+
+    IEnumerator SpawnCloud()
+    {
+        while (grounded)
+        {
+            Instantiate(dustCloud, transform.position, dustCloud.transform.rotation);
+            yield return new WaitForSeconds(0.25f);
+        }
+    }
+
+}
--- WGJ-142-Mischief/Assets/Scripts/CharacterController.cs
+++ WGJ-142-Mischief/Assets/Scripts/CharacterController.cs
@@ -4,7 +4,10 @@ using UnityEngine;
 
 public class CharacterController : MonoBehaviour
 {
-    private Rigidbody2D rb;
+    static protected CharacterController s_PlayerInstance;
+    static public CharacterController PlayerInstance { get { return s_PlayerInstance; } }
+
+    public Rigidbody2D rb;
     public const float DEFAULT_SPEED = 5;
     public const float RUNNING_SPEED = DEFAULT_SPEED + 3;
     public float speed = DEFAULT_SPEED;

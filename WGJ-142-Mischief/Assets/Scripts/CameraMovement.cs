aea2e71c0d8762ff0c33f9d38a0b8621f2d65815
Ethan <eitanwass@gmail.com>
2020-03-28 19:05:27 +0300

    Set base for camera control with mouse

--- WGJ-142-Mischief/Assets/Scripts/CameraMovement.cs
+++ WGJ-142-Mischief/Assets/Scripts/CameraMovement.cs
@@ -4,17 +4,36 @@ using UnityEngine;
 
 public class CameraMovement : MonoBehaviour
 {
+    private Camera mainCam;
+
     public GameObject followObject;
 
     public Vector3 offset;
 
     public float smoothFollowSpeed = 2f;
 
+    public Vector3 currentMousePosition;
 
+    public float cameraMovementRadius = 4.5f;
+
+
+
+    private void Awake()
+    {
+        mainCam = Camera.main;
+    }
+
+    private void Update()
+    {
+        Vector2 mousePos = Input.mousePosition;
+        currentMousePosition = mainCam.ScreenToWorldPoint(mousePos);
+        currentMousePosition.z = 0;
+    }
 
     private void LateUpdate()
     {
         Vector3 targetPosition = followObject.transform.position + offset;
+
         this.transform.position = Vector3.Lerp(transform.position, targetPosition, smoothFollowSpeed * Time.deltaTime);
     }
 }
--- WGJ-142-Mischief/Assets/Scripts/CharacterController.cs
+++ WGJ-142-Mischief/Assets/Scripts/CharacterController.cs
@@ -4,21 +4,29 @@ using UnityEngine;
 
 public class CharacterController : MonoBehaviour
 {
+    private Camera mainCam;
     private Rigidbody2D rb;
-    public const float DEFAULT_SPEED = 5;
-    public const float RUNNING_SPEED = DEFAULT_SPEED + 3;
-    public float speed = DEFAULT_SPEED;
+
+    public float walkingSpeed = 5;
+    public float runningSpeed = 8;
+
+    private float speed;
+
+
+    private Vector3 mousePosition;
 
 
     private void Awake()
     {
+        mainCam = Camera.main;
         rb = GetComponent<Rigidbody2D>();
     }
 
 
     private void Update()
     {
-
+        Vector2 mousePos = Input.mousePosition;
+        mousePosition = mainCam.ScreenToWorldPoint(new Vector2(mousePos.x, mousePos.y));
     }
 
     private void FixedUpdate()
@@ -39,15 +47,9 @@ public class CharacterController : MonoBehaviour
 
         //check if shift is pressed (shift is the key for running)
         if (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift))
-        {
-            speed = RUNNING_SPEED;
-        }
+            speed = runningSpeed;
         else
-        {
-            speed = DEFAULT_SPEED;
-        }
-
-        System.Console.WriteLine("speed: " + speed);
+            speed = walkingSpeed;
 
         Vector2 velocity = new Vector2(horizontal, vertical);
         velocity.Normalize();

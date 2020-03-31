d3dd95e2d96175c7d0b10e589e52871fb7be6712
benkir07 <benkir07@gmail.com>
2020-04-01 02:00:03 +0300

    Added A* algorithm
    Implemented on level 1 and tutorial and added prefab.

---
+++ WGJ-142-Mischief/Assets/Scripts/EnemyPathfinder.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+
+public class EnemyPathfinder : MonoBehaviour
+{
+
+    public float speed;
+    public float nextWaypointDistance = 3f;
+
+    private Path path;
+    int currentWaypoint = 0;
+
+    private Seeker seeker;
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+        seeker = GetComponent<Seeker>();
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    public void GoTo(Vector3 pos, bool scan)
+    {
+        if (scan)
+            AstarPath.active.Scan();
+        seeker.StartPath(rb.position, pos, OnPathComplete);
+    }
+
+    private void OnPathComplete(Path p)
+    {
+        if (!p.error)
+        {
+            path = p;
+            currentWaypoint = 0;
+        }
+    }
+
+    private void Update()
+    {
+        if (path == null)
+            return;
+
+        if (currentWaypoint >= path.vectorPath.Count)
+            return;
+
+        Vector2 direction = ((Vector2)path.vectorPath[currentWaypoint] - rb.position).normalized;
+        Vector2 force = direction * speed * Time.deltaTime;
+
+        rb.AddForce(force);
+
+        float distance = Vector2.Distance(rb.position, path.vectorPath[currentWaypoint]);
+
+        if (distance < nextWaypointDistance)
+        {
+            currentWaypoint++;
+        }
+
+    }
+}

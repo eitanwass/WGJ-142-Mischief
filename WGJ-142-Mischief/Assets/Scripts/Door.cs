3b84df95b391ed5d5950c700a1fc74faea80e36e
nadav ashkenazi <nadav1361@gmail.com>
2020-04-01 05:43:08 +0300

    Add interactable doors

---
+++ WGJ-142-Mischief/Assets/Scripts/Door.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : Interactble
+{
+    public GameObject door;
+
+    // Open/close a door.
+    public override void Interact()
+    {
+        door.SetActive(!door.active);
+    }
+}

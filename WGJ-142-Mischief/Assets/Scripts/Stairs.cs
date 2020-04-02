884fd9525a0729aab43ce449c9e8788ea2b122f1
benkir07 <benkir07@gmail.com>
2020-04-02 13:43:58 +0300

    fixed levels tutorial, 2 and 3

--- WGJ-142-Mischief/Assets/Scripts/Stairs.cs
+++ WGJ-142-Mischief/Assets/Scripts/Stairs.cs
@@ -9,16 +9,30 @@ public class Stairs : MonoBehaviour
     public GameObject floor1;
     public GameObject floor2;
 
+    private bool moved;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision == stairs)
+        {
             StartCoroutine(ChangeFloors());
+            moved = false;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision == stairs)
+            moved = true;
     }
 
     IEnumerator ChangeFloors()
     {
         yield return new WaitForSecondsRealtime(waitTime);
-        floor1.SetActive(!floor1.activeSelf);
-        floor2.SetActive(!floor2.activeSelf);
+        if (!moved)
+        {
+            floor1.SetActive(!floor1.activeSelf);
+            floor2.SetActive(!floor2.activeSelf);
+        }
     }
 }

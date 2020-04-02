d22739e7b9f1d4cb590505835bd4801533c98e5f
Adi Sneh <adisneh@gmail.com>
2020-04-02 18:48:57 +0300

    Change SantaTalking feature
    Add some real text to santatext.cs

--- WGJ-142-Mischief/Assets/Scripts/ScriptsSantaText/SantaText.cs
+++ WGJ-142-Mischief/Assets/Scripts/ScriptsSantaText/SantaText.cs
@@ -5,18 +5,53 @@ using UnityEngine;
 namespace SantaTextNamespace {
     public class SantaText : MonoBehaviour
     {
-        private static string[] santaTextArr = {
-            "Ho, Ho, Ho! I seem to have misplaced some of the Nice kids' presents in the Naughty kids' houses. I need your help to save Christmas! Are you up to the task?",
-            null,
-            "Sample Santa Text 1a",
-            "Sample Santa Text 1b",
-            null,
-            "Sample Santa Text 3a",
-            "Sample Santa Text 3b",
-            "Sample Santa Text 3c",
+        public static string[,] santaTextArr = {
+            {
+                "Ho, Ho, Ho! Oh no! I shouldn’t have added my “secret ingredient” to those milk glasses *hick*",
+                "I seem to have misplaced some of the Nice kids' presents in the Naughty kids' houses.",
+                "These kids have been nothing but mischievous all year long!",
+                "Please, my trusty little elf, help me get the gifts back before they wake up!",
+                null,
+                "Ok, now that you’ve reached the house, you must find all the presents I left.",
+                "We can't let a little mischievous child get presents, they must earn them by being nice all year long.",
+                null,
+                "Oh! Here’s one of them. Quick, run to that gift and replace it with coal.",
+                "Find and replace all of the gifts, and remember: don't get caught!",
+                null,
+                "In some houses the parents might be still up, you know... uhh... doing adult stuff, like... uhhhh... doing the dishes.",
+                "Anyways, try to stay out of their sight.",
+                null,
+                "Ho Ho Ho, maybe you can wash ol’ Saint Nick’s suit after this journey.",
+                ""
+            }/*,
+
+            {
+                "1",
+                null,
+                "11",
+                "111",
+                ""
+            },
+
+            {
+                "2",
+                null,
+                "22",
+                "222",
+                ""
+            },
+
+            {
+                "3",
+                null,
+                "33",
+                "333",
+                ""
+            }*/
+
         };
         
-        private static LinkedList<string> santaTextList = new LinkedList<string>(santaTextArr);
-        public static LinkedListNode<string> santaTextNode = santaTextList.First;
+        // private static LinkedList<string> santaTextList = new LinkedList<string>(santaTextArr);
+        // public static LinkedListNode<string> santaTextNode = santaTextList.First;
     }
 }

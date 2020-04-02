6e9a4f0aa15790a693137bbe0a398a79d5e779ca
hellokid1111 <34838527+hellokid1111@users.noreply.github.com>
2020-04-02 08:30:50 +0300

    Added music

---
+++ WGJ-142-Mischief/Assets/Scripts/Audio.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Audio : MonoBehaviour
+{
+    AudioSource m_MyAudioSource;
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_MyAudioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        m_MyAudioSource.volume = 1 * PlayerPrefs.GetFloat("Music");
+    }
+}
